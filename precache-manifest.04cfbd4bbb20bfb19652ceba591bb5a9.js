b5892a45b9648f5daaf1fae5c3dbf73845b536d6
Ssentongo-Mansoor <ssentongomansoor@gmail.com>
2020-10-21 16:32:16 +0300

    Updates

---
+++ precache-manifest.04cfbd4bbb20bfb19652ceba591bb5a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7fba46717dd87cd3669c315466f514",
+    "url": "/TrigaPay.io/index.html"
+  },
+  {
+    "revision": "6861017deb88dc91638d",
+    "url": "/TrigaPay.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d0463a6ee0d9b228c858",
+    "url": "/TrigaPay.io/static/js/2.0454eca6.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/TrigaPay.io/static/js/2.0454eca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6861017deb88dc91638d",
+    "url": "/TrigaPay.io/static/js/main.b48dbf91.chunk.js"
+  },
+  {
+    "revision": "6b351d30f4a987c07b6d",
+    "url": "/TrigaPay.io/static/js/runtime-main.aad9906e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TrigaPay.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
